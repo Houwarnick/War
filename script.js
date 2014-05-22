5cecd2848a6362b0477616b6694cceae43fce840
Houston <houwarnick@gmail.com>
2014-05-22 17:39:22 -0600

    working game

--- script.js
+++ script.js
@@ -27,17 +27,29 @@ $(document).ready(function() {
 			deck.push({number: j+1, suit: suit});
 		}
 	}
-	
 	//shuffle the deck
-	
+	deck = _.shuffle(deck);
 	
 	var cards_player_1 = [];
 	var cards_player_2 = [];
 	//divide out the cards into the two arrays
-	
+	for(var i = 0; i < deck.length; i++){
+		if(i <= 25){
+			cards_player_1.push(deck[i]);
+		}
+		else{
+			cards_player_2.push(deck[i]);
+		}
+	}
 	
 	//create a function (algorithm) called "war" that takes two cards as parameters, compares them and returns a winner. A tie should return false.
-	function war() {
+	function war(cardA, cardB) {
+		if(cardA.number > cardB.number)
+			return cardA;
+		else if(cardA.number < cardB.number)
+			return cardB;
+		else
+			return false
 	}
 	
 	
@@ -45,7 +57,21 @@ $(document).ready(function() {
 		//compare the cards
 		//give the winner both cards (at end of deck)
 	function play() {
-		
+		var playCard1 = cards_player_1.shift();
+		var playCard2 = cards_player_2.shift();
+		console.log(playCard1.number);
+		console.log(playCard2.number);
+		if(war(playCard1,playCard2)==playCard1){
+			cards_player_1.push(playCard1,playCard2);
+		}
+		else if(war(playCard1,playCard2)==playCard2){
+			cards_player_2.push(playCard1,playCard2);
+		}
+		else{
+			cards_player_1.push(playCard1);
+			cards_player_2.push(playCard2);
+		}
+
 		//this function (defined below) will continue to the next turn
 		advance();
 	}
